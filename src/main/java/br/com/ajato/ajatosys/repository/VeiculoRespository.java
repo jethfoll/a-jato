f85510ecab45159851b7ba5a2a803851a86f79c2
jethfoll <jefferson.silvadias94@gmail.com>
2021-02-17 19:01:50 -0300

    implementando veiculos

---
+++ src/main/java/br/com/ajato/ajatosys/repository/VeiculoRespository.java
@@ -0,0 +1,10 @@
+package br.com.ajato.ajatosys.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+
+import br.com.ajato.ajatosys.model.Veiculo;
+
+public interface VeiculoRespository extends JpaRepository<Veiculo, Long>{
+
+}
---
+++ src/main/java/br/com/ajato/ajatosys/resources/VeiculoResource.java
@@ -0,0 +1,23 @@
+package br.com.ajato.ajatosys.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ajato.ajatosys.model.Veiculo;
+import br.com.ajato.ajatosys.repository.VeiculoRespository;
+@RestController
+@RequestMapping(value="/ajato")
+public class VeiculoResource {
+
+	@Autowired
+	VeiculoRespository veiculoRespository;
+	
+	@GetMapping("/veiculo")
+	public List<Veiculo> listarVeiculos(){
+		return veiculoRespository.findAll();
+	}
+}
