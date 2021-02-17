bf0c0aba70fd7bc89e1b3e58dbd123e82aff9918
jethfoll <jefferson.silvadias94@gmail.com>
2021-02-17 18:24:43 -0300

    Rebase do projeto

---
+++ src/main/java/br/com/ajato/ajatosys/model/Veiculo.java
@@ -0,0 +1,45 @@
+package br.com.ajato.ajatosys.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_veiculo")
+public class Veiculo {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String placa;
+	private String cor;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+}
