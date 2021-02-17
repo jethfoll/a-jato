94f21c2c22fbd8c3985c1f3e07ef8a12bfd42ade
jethfoll <jefferson.silvadias94@gmail.com>
2021-02-17 15:43:15 -0300

    Rebase do projeto

---
+++ src/main/java/br/com/ajato/ajatosys/AjatosysApplication.java
@@ -0,0 +1,13 @@
+package br.com.ajato.ajatosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjatosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjatosysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ajato/ajatosys/AjatosysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ajato.ajatosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjatosysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
